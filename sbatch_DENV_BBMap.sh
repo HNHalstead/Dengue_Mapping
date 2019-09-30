1858a0de37aceb1587ed88281e7031cfa9fa945e
HNHalstead <halsteadholly73@gmail.com>
2019-09-30 09:43:10 +0200

    Better fit for RNA virus

--- sbatch_DENV_BBMap.sh
+++ sbatch_DENV_BBMap.sh
@@ -14,28 +14,32 @@
 module load bioinfo-tools
 module load bbmap/38.61b
 
+
+# Generate an index
 bbmap.sh ref=bb.genome.fasta
 
+# The index is written to the location /ref/
 
-"The index is written to the location /ref/"
 
 # Next step is to map
 
-bbmap.sh -Xmx100g threads=4 in1="$i"_1.fastq.gz in2="$i"_2.fastq.gz interleaved=true path=$db/bbmap maxindel=100k build=3 outu1="$i"_no.bb_1.fastq.gz outu2="$i"_no.bb_2.fastq.gz outm1="$i"_bb_1.fastq.gz outm2="$i"_bb_2.fastq.gz
+bbmap.sh threads=4 in1="$i"_1.fastq.gz in2="$i"_2.fastq.gz interleaved=true path=$db/bbmap maxindel=20k build=3 outu1="$i"_no.bb_1.fastq.gz outu2="$i"_no.bb_2.fastq.gz outm1="$i"_bb_1.fastq.gz outm2="$i"_bb_2.fastq.gz
+
+## threads changed to 4 from 32 as 32 was over-kill
+## maxindel reduced from 100k to 20k as Dengue isn't that large
+## -Xmx100g not needed with Uppmax
+
+#"Now the indel settings is for humans, not sure how this will work
+#for bedbugs. But give it a go."
+
+#“outu” = unmapped reads
+#“outm” = mapped reads
+
+#https://jgi.doe.gov/data-and-tools/bbtools/bb-tools-user-guide/bbmap-guide/
+#https://github.com/BioInfoTools/BBMap/blob/master/docs/UsageGuide.txt
 
 
-
-"Now the indel settings is for humans, not sure how this will work
-for bedbugs. But give it a go."
-
-“outu” = unmapped reads
-“outm” = mapped reads
-
-https://jgi.doe.gov/data-and-tools/bbtools/bb-tools-user-guide/bbmap-guide/
-https://github.com/BioInfoTools/BBMap/blob/master/docs/UsageGuide.txt
-
-
-You can do this as a shell script (save code below as map.away.bb.reads.sh or similar):
+#You can do this as a shell script (save code below as map.away.bb.reads.sh or similar):
 
 for file in *_R1.fastq.gz;
     do
@@ -44,6 +48,6 @@ for file in *_R1.fastq.gz;
 		base=${name%%_R1.fastq.gz}
         echo $base
 
-        bbmap.sh -Xmx100g threads=32 in1="$base"_R1.fastq.gz in2="$base"_R2.fastq.gz interleaved=true path=$db/bbmap 	maxindel=100k build=3 outu1="$base"_no.bb_R1.fastq.gz outu2="$base"_no.bb_R2.fastq.gz outm1="$base"_bb_R1.fastq.gz outm2="$base"_bb_R2.fastq.gz
+        bbmap.sh threads=4 in1="$base"_R1.fastq.gz in2="$base"_R2.fastq.gz interleaved=true path=$db/bbmap 	maxindel=20k build=3 outu1="$base"_no.bb_R1.fastq.gz outu2="$base"_no.bb_R2.fastq.gz outm1="$base"_bb_R1.fastq.gz outm2="$base"_bb_R2.fastq.gz
 
     done;
