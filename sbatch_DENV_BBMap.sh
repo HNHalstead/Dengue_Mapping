bbf4c87fb2f463fe0be416719fb768db574f5d61
HNHalstead <halsteadholly73@gmail.com>
2019-10-07 17:24:19 +0200

    test file created

--- sbatch_DENV_BBMap.sh
+++ sbatch_DENV_BBMap.sh
@@ -1,10 +1,9 @@
-
 #!/bin/bash -l
 #SBATCH -A snic2019-8-68
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -t 04:00:00
-#SBATCH -J hh_BBMap_DENV
+#SBATCH -J hh_BBMap_DENV_run1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user halsteadholly73@gmail.com
 
@@ -15,15 +14,16 @@ module load bioinfo-tools
 module load bbmap/38.61b
 
 
-"try .bbmap.sh if bbmap.sh doesnt work"
-bbmap.sh ref=bb.genome.fasta
+# try .bbmap.sh if bbmap.sh doesnt work"
+#bbmap.sh ref=bb.genome.fasta
+bbmap.sh ref=Denv1_cn_ref.fasta
 
 # The index is written to the location /ref/
 
 
 # Next step is to map
 
-bbmap.sh threads=4 in1="$i"_1.fastq.gz in2="$i"_2.fastq.gz interleaved=true path=$db/bbmap maxindel=20k build=3 outu1="$i"_no.bb_1.fastq.gz outu2="$i"_no.bb_2.fastq.gz outm1="$i"_bb_1.fastq.gz outm2="$i"_bb_2.fastq.gz
+# bbmap.sh threads=4 in1="$i"_1.fastq.gz in2="$i"_2.fastq.gz interleaved=true path=$db/bbmap maxindel=20k build=3 outu1="$i"_no.bb_1.fastq.gz outu2="$i"_no.bb_2.fastq.gz outm1="$i"_bb_1.fastq.gz outm2="$i"_bb_2.fastq.gz
 
 ## threads changed to 4 from 32 as 32 was over-kill
 ## maxindel reduced from 100k to 20k as Dengue isn't that large
@@ -45,7 +45,7 @@ for file in *_R1.fastq.gz;
     do
         name=${file##*/}
 		echo $name
-		base=${name%%_R1.fastq.gz}
+		base=${name%%_1.fastq.gz}
         echo $base
 
         bbmap.sh threads=4 in1="$base"_R1.fastq.gz in2="$base"_R2.fastq.gz interleaved=true path=$db/bbmap 	maxindel=20k build=3 outu1="$base"_no.bb_R1.fastq.gz outu2="$base"_no.bb_R2.fastq.gz outm1="$base"_bb_R1.fastq.gz outm2="$base"_bb_R2.fastq.gz
