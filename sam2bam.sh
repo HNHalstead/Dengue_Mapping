b9f4abd897918c79f338e056a43936b9e65950d7
Holly Halstead <hoha8653@rackham2.uppmax.uu.se>
2019-10-24 11:21:54 +0200

    test script using develpment node used

--- sam2bam.sh
+++ sam2bam.sh
@@ -1,37 +1,10 @@
-#!/bin/bash -l
-
-#convering SAM to BAM, sort and index BAM
-#usage ./sam2bam.sh <infile>
-#outputfile will be <infile_stem>.bam and <infile_stem>.bam.bai
-#rrbsmap_path=${0%/*}
-tmpbam=${1%.*}.tmp.bam
-outbam=${1%.*}.bam
-
-echo "Converting SAM to BAM ..."
-if [ ! -f $1 ]; then
-	echo "$1 does not exist."
-	exit 1
-fi
-samtools/samtools view -bS $1 > $tmpbam
-if [ $? -ne 0 ]; then
-	echo "SAM2BAM conversion not successful."
-	echo "$1 remains unchanged."
-	rm $tmpbam
-	exit 1
-fi
-echo "Sorting BAM ..."
-samtools/samtools sort $tmpbam ${outbam%.*}
-if [ $? -ne 0 ]; then
-	echo "BAM file sorting not successful."
-	echo "$outbam is in unsorted BAM format".
-	mv $tmpbam $outbam
-	exit 1
-fi
-rm $tmpbam
-echo "Indexing BAM ..."
-samtools/samtools index $outbam
-if [ $? -ne 0 ]; then
-	echo "BAM file indexing not successful."
-	exit 1
-fi
-exit 0
+#!/bin/bash
+echo "Note: This script is designed to run with the amount of memory detected by BBMap."
+echo "      If Samtools crashes, please ensure you are running on the same platform as BBMap,"
+echo "      or reduce Samtools' memory setting (the -m flag)."
+echo "Note: Please ignore any warnings about 'EOF marker is absent'; this is a bug in samtools that occurs when using piped input."
+samtools view -bShu MM16_FDMS190687288-1a_HMWVWDSXX_L3_bb.sam | samtools sort -m 17G -@ 3 - -o MM16_FDMS190687288-1a_HMWVWDSXX_L3_bb_sorted.bam
+samtools index MM16_FDMS190687288-1a_HMWVWDSXX_L3_bb_sorted.bam
+echo "Note: Please ignore any warnings about 'EOF marker is absent'; this is a bug in samtools that occurs when using piped input."
+samtools view -bShu MM16_FDMS190687288-1a_HMWVWDSXX_L3_bb_un.sam | samtools sort -m 17G -@ 3 - -o MM16_FDMS190687288-1a_HMWVWDSXX_L3_bb_un_sorted.bam
+samtools index MM16_FDMS190687288-1a_HMWVWDSXX_L3_bb_un_sorted.bam
